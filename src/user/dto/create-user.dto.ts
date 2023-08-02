2ff512d1548c18ff59babec146d1a389b90ada81
BurningProcessor <>
2023-08-02 20:11:35 +0000

    add creating user
    FossilOrigin-Name: 775224c2c5d56e0f46734f6f6c9ab89b08fee8dbee00cf4ad6ce99f01bd473f9

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, MinLength } from "class-validator";
+
 export class CreateUserDto {
+
+    @IsEmail()
     email: string;
+
+    @MinLength(6, {message: 'Pass must be 6 or more symbols'})
     password: string;
 }
